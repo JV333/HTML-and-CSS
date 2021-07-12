97adcc257926bd8515a3439ea682a78151b5bad5
JV333 <86992368+JV333@users.noreply.github.com>
2021-07-12 18:07:08 -0500

    added Basic_HTML_1
    basic function practice

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -2,3 +2,9 @@ function display()
  {
 alert("Greetings meager human, I am made of 1s and 0s");
  }
+ function My_First_Function() {
+	var String = "What's up man";
+	var result = String.fontcolor("purple");
+	document.getElementById("Greeting").innerHTML = 
+	result;
+	}
